1e7b91affc67b7744aeeee3561a90d639c7c75e7
NiksanaNikolov85 <nikola.nikolov3398@gmail.com>
2019-10-02 17:57:26 +0300

    add_examples

---
+++ scopes/this.js
@@ -0,0 +1,14 @@
+let John = {
+    name: 'John',
+    year: 1980,
+    calclulateAge: function(){
+        console.log(this);
+        console.log(2016 - this.year);
+
+        function innerFunction(){
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+John.calclulateAge();

3395bd883e10dfb4078543e6402287057746834b
NiksanaNikolov85 <55994547+NiksanaNikolov85@users.noreply.github.com>
2019-10-07 16:14:36 +0300

    initialized package.json (#3)
    Setting nodemon

--- project/index.js
+++ project/index.js
@@ -1,10 +1,13 @@
-function sayHello(name){
-    console.log('Hello '+name);
-}
+var express = require('express')
+var app = express()
 
-sayHello('Ben');
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world !')
+})
 
-let logger =require('./logger'); 
-console.log(logger);
+app.get('/test', function (req, res) {
+    res.send('Hello again !')
+})
 
-logger.log('message 1');
+app.listen(3000, () => {console.log('Started again')})

c3751a8ae93964b14b57bb9a43048037e539db81
NiksanaNikolov85 <55994547+NiksanaNikolov85@users.noreply.github.com>
2019-10-09 13:39:20 +0300

    Weather ip (#4)
    * setting new url
    
    * request city weather
    
    * weather IP
    
    * Weather IP update
    
    * Update weather IP
    
    * adding arrow function

--- project/index.js
+++ project/index.js
@@ -1,5 +1,7 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
@@ -8,6 +10,38 @@ app.get('/', function (req, res) {
 
 app.get('/test', function (req, res) {
     res.send('Hello again !')
+})
+
+app.get('/user/:id', function (req, res) {
+    res.send('user ' + req.params.id)
+})
+
+app.get('/posts', function (req, res) {
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then((result) => {
+            console.log(result.data)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result.data));
+        });
+})
+
+app.get('/posts/:id', function (req, res) {
+    const postId = req.params.id;
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+        .then((result) => {
+            console.log(result.data)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result.data));
+        });
+})
+
+app.get('/vreme/:city', function (req, res) {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&APPID=4b6cc4183d2ef6e1e65f303e83f3d509`)
+        .then((result) => {
+            console.log(result.data)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result.data));
+        });
 })
 
 app.listen(3000, () => {console.log('Started again')})
---
+++ weather_ip/index.js
@@ -0,0 +1,49 @@
+let printMsg = function (){
+    console.log('Show weather forecast');
+}
+printMsg();
+
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const APPID = "4b6cc4183d2ef6e1e65f303e83f3d509";
+app.get('/', function (req, res) {
+    res.send('Hello again !!!!!')
+})
+
+app.get('/test', function (req, res) {
+    res.send('Hello again !')
+})
+
+app.get('/posts', (req, res) => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then((result) => {
+            console.log(result.data)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result.data));
+        });
+})
+
+app.get('/posts/:city', (req, res) => {
+    axios.get('https://api.openweathermap.org/data/2.5/sdweather', {
+        params: {
+            q: req.params.city,
+            APPID,
+        }
+    }).then((result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result.data));
+    }).catch((error) => {
+        const code = error.response && error.response.data && error.response.data.cod;
+        const statusCode = error.response.data.cod;
+
+        if (code ==='404') {
+            res.status(statusCode).send('Something broke! There is error '+statusCode);
+        } else {
+            res.status(statusCode).send('Something broke which is error :' +statusCode);
+        }
+    });
+});
+
+app.listen(8080, () => {console.log('Started again')})
